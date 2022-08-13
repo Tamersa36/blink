c6bcaa8de412985ea6ea9f54433084030fff50a9
Tamersa36 <103407092+Tamersa36@users.noreply.github.com>
2022-08-13 21:11:52 +0300

    rebuild logic

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from "src/app/services/post.service";
+import { Order } from 'src/app/models/Order';
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +9,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  orders:Order[] = [];
+  order: Order | any;
+  private ordersSub: Subscription = new Subscription;
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.fetchOrders()
+    setInterval(() => this.fetchOrders(), 5000);
   }
+  ngOnDestroy(): void {
+    this.ordersSub.unsubscribe();
+  }
+
+  fetchOrders(){
+    debugger;
+      console.log("1",this.orders);
+      this.postService.getOrder();
+      this.ordersSub = this.postService.getOrdUpdateListener()
+      .subscribe((request)=>{
+        this.raw(request);
+      });
+      }
+
+      raw(req: any){
+        // this.order = req.order
+        if(req.order){
+      this.orders.push(req.order)
+      console.log(this.orders);
+  }
+  }
+    //   fetchOrders(){
+    // // let interval = setInterval(() => {
+    //   console.log(this.orders);
+    //   this.postService.getOrder();
+    //   this.ordersSub = this.postService.getOrdUpdateListener()
+    //   .subscribe((request)=>{
+    //     // this.raw(this.obj)
+    //     this.raw(request);
+    //     // this.order = order;
+    //     // console.log("fetched from dashboard this order: ", this.order)
+    //   });
+    //   // }, 5000)
+    //   //   let interval = setInterval(() => {
+    //     //     this.postService.getOrders
+    //     // }, 2000)
+    //   }
+
+
 
 }
