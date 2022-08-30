c0bb9f2e930fdb53c15079e88d902a569709dc3f
Tamersa36 <103407092+Tamersa36@users.noreply.github.com>
2022-08-31 02:59:13 +0300

    remove state

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -26,19 +26,19 @@ export class DashboardComponent implements OnInit {
       this.router.navigateByUrl('/login');
 
     //saving and fetching dashboard state
-    const ordersState = this.postService.loadState('orders');
-    if (ordersState) {
-      ordersState.forEach((element: Order) => {
-        this.orders.push(element);
-      });
-    }
+    // const ordersState = this.postService.loadState('orders');
+    // if (ordersState) {
+    //   ordersState.forEach((element: Order) => {
+    //     this.orders.push(element);
+    //   });
+    // }
 
-    const tableState = this.postService.loadState('tables');
-    if (tableState) {
-      tableState.forEach((element: Table) => {
-        this.tables.push(element);
-      });
-    }
+    // const tableState = this.postService.loadState('tables');
+    // if (tableState) {
+    //   tableState.forEach((element: Table) => {
+    //     this.tables.push(element);
+    //   });
+    // }
 
     //main logic
     //listening for orders from backend and Database
